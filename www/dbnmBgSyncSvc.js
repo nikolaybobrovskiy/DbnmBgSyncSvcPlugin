86574a35c3d3cc255700f87f4b9948ff465724c7
Nikolay Bobrovskiy <nikolay.bobrovskiy@gmail.com>
2015-05-26 23:03:19 +0300

    Initial sync.

---
+++ www/dbnmBgSyncSvc.js
@@ -0,0 +1,4 @@
+var serviceName = 'com.dbn.plugin.MyService';
+
+var factory = require('com.red_folder.phonegap.plugin.backgroundservice.DbnmBgSyncSvc')
+module.exports = factory.create(serviceName);
